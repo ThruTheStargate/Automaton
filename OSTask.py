32ec60833a019f18772d27b254d081a4412e7a78
ThruTheStargate <kaia.aydelotte@gmail.com>
2026-09-03 12:36:14 -0600

    Initial restructure for multi-OS functionality, created OSTask class

---
+++ OSTask.py
@@ -0,0 +1,23 @@
+#OSTask - Created by Kaia Aydelotte, 09.03.26
+#Class designed to handle differences in operating systems
+#Last updated 09.03.26
+
+import os           #allows program to interact with Windows operating system
+import subprocess   #allows program to interact with Linux operating system
+
+class OSTask:
+
+    def openFile(self, filename):
+        ...
+
+    def switchWindow(self):
+        ...
+
+    def copy(self):
+        ...
+
+    def paste(self):
+        ...
+
+    def emergencyStop(self):
+        ...
